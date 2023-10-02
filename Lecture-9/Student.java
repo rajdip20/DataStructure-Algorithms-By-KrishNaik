9eb4635b61b4e86d4c8b3c2ef58d409482994807
Rajdip Das <dasrajdip78754282@gmail.com>
2023-10-02 19:35:51 +0530

    Initial commit

---
+++ Lecture-9/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int roll_no;
+    String name;
+
+    public static void main(String[] args) {
+        
+        Student obj1 = new Student();
+        obj1.roll_no = 2;
+        obj1.name = "John";
+        System.out.println(obj1.roll_no);
+        System.out.println(obj1.name);
+    }
+}
