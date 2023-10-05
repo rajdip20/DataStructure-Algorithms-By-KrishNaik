f01f5308d34169c1fb479701010fefe6411e36d5
Rajdip Das <dasrajdip78754282@gmail.com>
2023-10-05 13:47:01 +0530

    Initial commit

--- Lecture-1/RectangularArea.java
+++ Lecture-1/RectangularArea.java
@@ -1,7 +1,7 @@
-public class Area {
+public class RectangularArea {
     public static void main(String[] args) {
         int length = 7, breadth = 4;
         System.out.println("The Length is = " + length + " and the Breadth is = " + breadth);
-        System.out.println("The area is = " + length*breadth);
+        System.out.println("The area is = " + length * breadth);
     }
 }
