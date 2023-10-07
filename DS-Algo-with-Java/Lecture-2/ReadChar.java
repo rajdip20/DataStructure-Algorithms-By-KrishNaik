6feaf04ef9512e676241f50f1bc02c6c59819266
Rajdip Das <dasrajdip78754282@gmail.com>
2023-10-07 18:58:21 +0530

    Initial commit

--- DS-Algo-with-Java/Lecture-2/ReadChar.java
+++ DS-Algo-with-Java/Lecture-2/ReadChar.java
@@ -4,7 +4,7 @@ public class ReadChar {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             
-            System.out.println("Enter a Charecter: ");
+            System.out.print("Enter a Charecter: ");
             char ch = sc.next().charAt(0);
 
             System.out.println("Charecter is: " + ch);
