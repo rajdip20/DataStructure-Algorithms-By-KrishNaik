051e9b4fa3e64621135c157277167525b3e960ac
Rajdip Das <dasrajdip78754282@gmail.com>
2023-10-16 20:10:16 +0530

    Initial commit

---
+++ DS-Algo-with-Java/Lecture-4/EqualToThird.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EqualToThird {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Input the first number: ");
+            int a = sc.nextInt();
+            System.out.print("Input the second number: ");
+            int b = sc.nextInt();
+            System.out.print("Input the third number: ");
+            int c = sc.nextInt();
+
+            if (a + b == c) {
+                System.out.println("The result is: true");
+            } else {
+                System.out.println("The result is: false");
+            }
+        }
+    }
+}
