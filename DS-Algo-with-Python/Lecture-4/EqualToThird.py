b963884ce0931db921495e3a31cff084d7afd863
Rajdip Das <dasrajdip78754282@gmail.com>
2023-10-16 20:53:26 +0530

    Initial commit

---
+++ DS-Algo-with-Python/Lecture-4/EqualToThird.py
@@ -0,0 +1,8 @@
+a = int(input("Input the first number: "))
+b = int(input("Input the second number: "))
+c = int(input("Input the third number: "))
+
+if a + b == c:
+    print("The result is: true")
+else:
+    print("The result is: false")
