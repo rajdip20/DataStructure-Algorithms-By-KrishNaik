369a5ab4d1bd1584b611442e81ecf859bf165142
Rajdip Das <dasrajdip78754282@gmail.com>
2023-11-14 14:55:37 +0530

    Initial commit

---
+++ DS-Algo-with-Java/Lecture-12 Arrays/PrintPositiveInArray.java
@@ -0,0 +1,11 @@
+public class PrintPositiveInArray {
+    public static void main(String[] args) {
+        int arr[] = {2, 6, -5, -1, 0, 4, -9};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
